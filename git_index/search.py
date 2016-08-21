86207d6eff3846985c0843800b162cc56eae7389
Mark McGuire <mark.b.mcg@gmail.com>
2016-08-20 22:17:56 -0500

    only show 5 lines of context

--- git_index/search.py
+++ git_index/search.py
@@ -1,12 +1,23 @@
 import curses
+import itertools
 from elasticsearch_dsl import Q, Search
+
+
+def line_offsets(hit):
+    return {ih.meta.nested.offset for ih in hit.meta.inner_hits.lines}
+
+
+def relevant_line_numbers(lines, offsets, context):
+    return set(itertools.chain.from_iterable(range(max(0, o - context), min(o + context, len(lines)))
+                                             for o in offsets))
 
 
 class HitsWindow:
     def __init__(self, screen, hits, context=5):
         self.screen = screen
         self.hits = hits
-        self.rows = sum(len(h.lines) + 2 for h in hits)
+        self.context = context
+        self.rows = sum(len(relevant_line_numbers(h.lines, line_offsets(h), context)) + 2 for h in hits)
         self.cols = max(len(l.content) for h in hits for l in h.lines)
         self.top = 0
         self.left = 0
@@ -49,15 +60,17 @@ class HitsWindow:
     def add_hit(self, y, hit):
         self.pad.addstr(y, 0, 'commit {}'.format(hit.commit_sha), curses.color_pair(1))
         self.pad.addstr(y + 1, 0, 'path /{}'.format(hit.path), curses.A_BOLD)
-        inner_hit_offsets = set(ih.meta.nested.offset for ih in hit.meta.inner_hits.lines)
-        for i, l in enumerate(hit.lines):
-            color = curses.color_pair(3) if l.type == '+' else curses.color_pair(2)
-            line = l.type + l.content.rstrip()
-            if i in inner_hit_offsets:
-                self.pad.addstr(y + 2 + i, 0, line, color | curses.A_BOLD)
+        offsets = line_offsets(hit)
+        line_nums = relevant_line_numbers(hit.lines, offsets, self.context)
+        for i, line_pos in enumerate(sorted(line_nums)):
+            line = hit.lines[line_pos]
+            color = curses.color_pair(3) if line.type == '+' else curses.color_pair(2)
+            text = line.type + line.content.rstrip()
+            if line_pos in offsets:
+                self.pad.addstr(y + 2 + i, 0, text, color | curses.A_BOLD)
             else:
-                self.pad.addstr(y + 2 + i, 0, line, color)
-        return y + 2 + len(hit.lines)
+                self.pad.addstr(y + 2 + i, 0, text, color)
+        return y + 2 + len(line_nums)
 
 
 def search(query):
@@ -68,11 +81,17 @@ def search(query):
 
 
 def display(stdscr, hits):
-    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(False)
-    hits_window = HitsWindow(stdscr, hits)
-    hits_window.display()
+    try:
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(False)
+        hits_window = HitsWindow(stdscr, hits)
+        hits_window.display()
+    finally:
+        curses.curs_set(1)
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
